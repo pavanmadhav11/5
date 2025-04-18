7120f1b51dcfb31127a26045ae9958f3417f4280
pavanmadhav11 <pavanchowdary851@gmail.com>
2025-04-18 16:46:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from converter import python_to_mermaid
+import os
 
 app = Flask(__name__)
 
@@ -13,4 +14,5 @@ def index():
     return render_template("index.html", code=code, mermaid_code=mermaid_code)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
